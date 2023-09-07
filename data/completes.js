5660b4510fbb9124d8ed20a7bb92c6c8f99db6df
ANDRESWEB <andresamorin1@gmail.com>
2023-09-07 20:00:28 -0300

    Agrego completes

---
+++ data/completes.js
@@ -0,0 +1,47 @@
+{
+    'name': 'STRAND SQUALL',
+    'description': `THE DEEP COLLECTION
+
+    It/s time to broaden your perspective with a trip down below, to The Deep. Our friend Nate Reifke, a.k.a. Salty Timbers, is back with some mind-bending graphics from the abyss for the all new The Deep Collection. Reach to the depths of your senses, with Nate’s signature style of black and white illustration positioned over colorful splattered backdrops. To top it off, or should we say bottom it off, we’ve taken these boards even deeper with exposed Black Burle veneers. While you’re all probably familiar with the Fat Wave and Fault Line shapes, we’re also introducing the Strand for the first time ever in a 7 ply Maple construction. Some vivid graphics on some solid shapes are sure to make all your trips wild with The Deep Collection.
+    
+     
+    
+    RIDING STYLE
+    
+    Carving
+    Cruiser
+     
+    
+    DIMENSIONS
+    
+    Length: 34.0”
+    Width: 8.7”
+    Wheelbase: 20.5”
+     
+    
+    COMPONENTS
+    
+    8.375” Gullwing Mission Trucks
+    61mm 78a Nineballs
+    ABEC 5 Greaseball Bearings
+    0.25” Recycled Plastic Risers
+    1.25” Hardened Steel Bolts
+    Custom Screened Grip Tape
+     
+    
+    FEATURES
+    
+    7 Ply Maple
+    Black Burle Bottom Veneer
+    Two-Tone Swirled Wheels
+    Printed Grip Tape
+    Kicktail
+    Art by Nate Reifke`
+    ,
+    'picture': 'STRANDSQUAL01_2048x.webp',
+    'price': '198.95',
+    'stock': '89',
+    'category': 'completes',
+    'featured': false,
+    // slug: 
+}
