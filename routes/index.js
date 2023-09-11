7634b29895b7ff2fb84af39c8b68a0adaf81fa55
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-11 11:07:08 -0300

    Product Controller y Routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const productRoutes = require("./productRoutes");
-const categoryRoutes = require("./categoryRoutes");
+const productRoutes = require('./productRoutes');
+const categoryRoutes = require('./categoryRoutes');
 
 module.exports = (app) => {
-  app.use("/categories", categoryRoutes);
-  // app.use("/products", productRoutes);
+  app.use('/categories', categoryRoutes);
+  app.use('/products', productRoutes);
   //   app.use("/order", orderRoutes);
 };
