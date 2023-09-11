abbb3c5b156cb3d552fc7d9cfc1377a52f18c55b
GabrielLaborda <gabriel.laborda.fsd@gmail.com>
2023-09-11 11:56:28 -0300

    userRoutes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const productRoutes = require("./productRoutes");
 const categoryRoutes = require("./categoryRoutes");
+const userRoutes = require("./usersRoutes");
 
 module.exports = (app) => {
   app.use("/categories", categoryRoutes);
+  app.use("/user", userRoutes);
   // app.use("/products", productRoutes);
   //   app.use("/order", orderRoutes);
 };
