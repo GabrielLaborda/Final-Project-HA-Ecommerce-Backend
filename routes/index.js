760e9654c9653e301a361decda2097f7575a4f3c
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-16 12:39:32 -0300

    Update backend

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
-const productRoutes = require("./productRoutes");
-const categoryRoutes = require("./categoryRoutes");
-const userRoutes = require("./usersRoutes");
-const adminRoutes = require("./adminRoutes");
-const loginRoutes = require("./loginRoutes");
+const productRoutes = require('./productRoutes');
+const categoryRoutes = require('./categoryRoutes');
+const userRoutes = require('./usersRoutes');
+const adminRoutes = require('./adminRoutes');
+const loginRoutes = require('./loginRoutes');
 
 module.exports = (app) => {
-  app.use("/login", loginRoutes);
-  app.use("/products", productRoutes);
-  app.use("/admin", adminRoutes);
-  app.use("/users", userRoutes);
+  app.use('/login', loginRoutes);
+  app.use('/categories', categoryRoutes);
+  app.use('/products', productRoutes);
+  app.use('/admin', adminRoutes);
+  app.use('/users', userRoutes);
   //   app.use("/order", orderRoutes);
 };
