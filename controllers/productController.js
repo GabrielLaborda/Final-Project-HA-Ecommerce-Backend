e4e5faad6c6896b33536e5c9c6e1e9919b598880
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-18 16:32:37 -0300

    Agregamos lógica para crear productos con sus correspondientes fotos

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,8 @@ const Category = require('./../models/Category');
 const formidable = require('formidable');
 const slugify = require('slugify');
 const fs = require('fs');
+const path = require('path');
+const supabase = require('./../supabaseConfig');
 
 async function index(req, res) {
   try {
@@ -38,17 +40,40 @@ async function show(req, res) {
 async function store(req, res) {
   const form = formidable({
     multiples: true,
-    uploadDir: __dirname + '/../public/img',
     keepExtensions: true,
   });
 
   form.parse(req, async (err, fields, files) => {
-    const picturesArray = [];
-    files.picture.newFilename
-      ? picturesArray.push(files.picture.newFilename)
-      : picturesArray.push(...files.picture.map((picture) => picture.newFilename));
-
+    // const ext = path.extname(files.avatar.filepath);
+    // const newFileName = `image_${Date.now()}${ext}`;
     try {
+      const picturesArray = [];
+
+      if (files.picture.newFilename) {
+        picturesArray.push(files.picture.newFilename);
+        const { data, error } = await supabase.storage
+          .from('UrbanRush')
+          .upload(files.picture.newFilename, fs.createReadStream(files.picture.filepath), {
+            cacheControl: '3600',
+            upsert: false,
+            contentType: files.picture.mimetype,
+            duplex: 'half',
+          });
+        console.log(data);
+        console.log(error);
+      } else {
+        picturesArray.push(...files.picture.map((picture) => picture.newFilename));
+        for (const picture of files.picture) {
+          const { data, error } = await supabase.storage
+            .from('UrbanRush')
+            .upload(picture.newFilename, fs.createReadStream(picture.filepath), {
+              cacheControl: '3600',
+              upsert: false,
+              contentType: picture.mimetype,
+              duplex: 'half',
+            });
+        }
+      }
       const product = new Product({
         name: fields.name,
         description: fields.description,
@@ -66,15 +91,56 @@ async function store(req, res) {
       res.status(201).json({ msg: 'product successfully created' });
     } catch (error) {
       console.log('[ Product Controller -> Store ] Ops, something went wrong');
-      for (const picture of files.picture) {
-        fs.unlink(__dirname + '/../public/img/' + picture.newFilename, (err) => {
-          if (err) console.log(err);
-        });
-      }
+      // for (const picture of files.picture) {
+      //   fs.unlink(__dirname + '/../public/img/' + picture.newFilename, (err) => {
+      //     if (err) console.log(err);
+      //   });
+      // }
       return res.status(400).json({ msg: error.message });
     }
   });
 }
+
+// async function store(req, res) {
+//   const form = formidable({
+//     multiples: true,
+//     uploadDir: __dirname + '/../public/img',
+//     keepExtensions: true,
+//   });
+
+//   form.parse(req, async (err, fields, files) => {
+//     const picturesArray = [];
+//     files.picture.newFilename
+//       ? picturesArray.push(files.picture.newFilename)
+//       : picturesArray.push(...files.picture.map((picture) => picture.newFilename));
+
+//     try {
+//       const product = new Product({
+//         name: fields.name,
+//         description: fields.description,
+//         picture: picturesArray,
+//         price: fields.price,
+//         stock: fields.stock,
+//         category: fields.category,
+//         featured: fields.featured,
+//         slug: slugify(fields.name, { lower: true, strict: true }),
+//       });
+//       await product.save();
+//       const category = await Category.findById(fields.category);
+//       category.products.push(product._id);
+//       await category.save();
+//       res.status(201).json({ msg: 'product successfully created' });
+//     } catch (error) {
+//       console.log('[ Product Controller -> Store ] Ops, something went wrong');
+//       for (const picture of files.picture) {
+//         fs.unlink(__dirname + '/../public/img/' + picture.newFilename, (err) => {
+//           if (err) console.log(err);
+//         });
+//       }
+//       return res.status(400).json({ msg: error.message });
+//     }
+//   });
+// }
 
 async function update(req, res) {
   if (req.query.transaction === 'buy') {
