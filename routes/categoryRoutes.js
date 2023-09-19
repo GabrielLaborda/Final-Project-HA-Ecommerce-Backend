fa3c06c4ffec663d10d20c78259385dc67f10ca0
GonzaloCuello01 <gonzacuello-farq@hotmail.com>
2023-09-19 20:28:09 -0300

    Ajuste de rutas de Category
    Se protegen rutas de Category (store, patch, delete)

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,8 +4,20 @@ const categoryController = require("../controllers/categoryController");
 
 router.get("/", categoryController.index);
 router.get("/:slug", categoryController.show);
-router.post("/", categoryController.store);
-router.patch("/:slug", categoryController.update);
-router.delete("/:slug", categoryController.destroy);
+router.post(
+  "/",
+  checkJwt({ secret: process.env.JWT_SIGN_SECRET, algorithms: ["HS256"] }),
+  categoryController.store
+);
+router.patch(
+  "/:slug",
+  checkJwt({ secret: process.env.JWT_SIGN_SECRET, algorithms: ["HS256"] }),
+  categoryController.update
+);
+router.delete(
+  "/:slug",
+  checkJwt({ secret: process.env.JWT_SIGN_SECRET, algorithms: ["HS256"] }),
+  categoryController.destroy
+);
 
 module.exports = router;
