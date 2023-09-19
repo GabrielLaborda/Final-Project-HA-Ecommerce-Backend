df9eed99c0c589b2967b6df793f95792928bc7d1
GonzaloCuello01 <gonzacuello-farq@hotmail.com>
2023-09-19 20:32:27 -0300

    Revert "Ajuste de rutas de Category"
    This reverts commit fa3c06c4ffec663d10d20c78259385dc67f10ca0.

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,20 +4,8 @@ const categoryController = require("../controllers/categoryController");
 
 router.get("/", categoryController.index);
 router.get("/:slug", categoryController.show);
-router.post(
-  "/",
-  checkJwt({ secret: process.env.JWT_SIGN_SECRET, algorithms: ["HS256"] }),
-  categoryController.store
-);
-router.patch(
-  "/:slug",
-  checkJwt({ secret: process.env.JWT_SIGN_SECRET, algorithms: ["HS256"] }),
-  categoryController.update
-);
-router.delete(
-  "/:slug",
-  checkJwt({ secret: process.env.JWT_SIGN_SECRET, algorithms: ["HS256"] }),
-  categoryController.destroy
-);
+router.post("/", categoryController.store);
+router.patch("/:slug", categoryController.update);
+router.delete("/:slug", categoryController.destroy);
 
 module.exports = router;
