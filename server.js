5327ac89d8978d5649a391d13ec4c53656e90497
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-20 10:18:12 -0300

    Saco lo de access control headers

--- server.js
+++ server.js
@@ -13,14 +13,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  next();
-});
-
 app.use(express.json());
 routes(app);
 
