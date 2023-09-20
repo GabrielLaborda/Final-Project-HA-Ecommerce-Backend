2787622581b2148947e5816924df8343e349f557
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-20 11:16:39 -0300

    Cambios server

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ const routes = require('./routes');
 
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
-
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(cors());
-
 app.use(express.json());
 routes(app);
 
