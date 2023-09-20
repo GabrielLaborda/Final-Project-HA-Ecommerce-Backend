eb952c8cdc0abccbc2084e4a94e55400ba24ad87
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-20 10:15:29 -0300

    Update server

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
+
 app.use(express.json());
 routes(app);
 
